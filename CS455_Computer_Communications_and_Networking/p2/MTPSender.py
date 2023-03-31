65c7d42e68af1c3dd746bde9f8cde7a55dcee598
AmitKhadka <99443146+AmitKhadka@users.noreply.github.com>
2023-03-31 14:50:48 -0400

    mtp_sender

--- CS455_Computer_Communications_and_Networking/p2/MTPSender.py
+++ CS455_Computer_Communications_and_Networking/p2/MTPSender.py
@@ -3,35 +3,81 @@
 
 
 ## import (add more as you need)
+import numpy as np
 import threading
 import unreliable_channel
+import zlib
+import socket
 
 
-## define and initialize
-# window_size, window_base, seq_number, dup_ack_count, etc.
+PACKET_SIZE = 1456
+# define and initialize
+window_base = 0
+window_size = 0
+max_window_size = 0
+dup_ack_count = 0
+next_seq_number = 0
 
+mutex = threading.Lock()
+
+sender_log = None
+
+def int_to_bytes(i):
+	return np.int64(i).tobytes()
+
+def bytes_to_int (b):
+	return np.frombuffer(b, dtype=np.uint32)[0]
+
+def calc_checksum(b1, b2, b3):
+	checksum = 0
+	if type(b1) is int:
+		b1 = np.int64(b1).tobytes()
+		b2 = np.int64(b2).tobytes()
+		b3 = np.int64(b3).tobytes()
+		checksum = zlib.crc32(b1+b2+b3)
+	return int_to_bytes(checksum)
 
 ## we will need a lock to protect against two concurrent threads
 lock = threading.Lock()
 
 
-def create_packet(..):
+def create_packet(data, seq_num):
 # create data packet
 # crc32 available through zlib library
+	type_data = b'data'  
+	seq_num = int_to_bytes(seq_num)
+	data_length = int_to_bytes(len(data) + 16)
+	checksum = calc_checksum(type_data, seq_num, data_length)
+	data = bytes(data, 'utf-8')
+	return type_data + seq_num + data_length + checksum + data
 
-
-def extract_packet_info(..):
+def extract_packet_info(packet):
 # extract the ack after receiving
+	mtp_header = packet[0:16]
+	type_data = bytes_to_int(mtp_header[0:4])
+	seq_num = bytes_to_int(mtp_header[4:8])
+	data_length = bytes_to_int(mtp_header[8:12])
+	checksum = bytes_to_int(mtp_header[12:16])
+	return type_data, seq_num, data_length, checksum
 
 
-def receive_thread(..):
+def receive_thread(s_socket):
 	while True:
 		# receive ack, but using our unreliable channel
 		# packet_from_receiver, receiver_addr = unreliable_channel.recv_packet(socket)
 		# call extract_packet_info
 		# check for corruption, take steps accordingly
 		# update window size, timer, triple dup acks
+		packet_from_server, server_addr = unreliable_channel.recv_packet(s_socket)
+		type_data, next_seq_num, data_length, checksum = extract_packet_info(packet_from_server)
 
+		if next_seq_num == 0:
+			break
+		validate_checksum = bytes_to_int(calc_checksum(type_data, next_seq_num, data_length))
+		if checksum != validate_checksum:
+			sender_log.write()
+def decode_header(..):
+    return
 
 def main(..):
 	# some of the things to do:
